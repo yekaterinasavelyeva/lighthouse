64d11d098b9d9fbed790ab28a2b8f1ed05d8673a
Jekaterina Saveljeva <katya-a@inbox.lv>
2017-04-09 18:15:36 +0300

    add more tests for reservation validator

--- src/test/java/lv/javaguru/java2/services/validate/ReservationValidatorImplTest.java
+++ src/test/java/lv/javaguru/java2/services/validate/ReservationValidatorImplTest.java
@@ -33,6 +33,30 @@ public class ReservationValidatorImplTest {
     public void shouldThrowExceptionWhenDateFormatIsWrong() {
              }
 
+    @Test
+    public void shouldThrowExceptionWhenReservationFromDateIsWrong() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Reservation Start Date must be set for today!");
+        validator.validate(LocalDate.now().plusDays(2), LocalDate.now().plusDays(8), 12345l);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenReservationToDateIsWrong() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Reservation End Date must be set for no less " +
+                "than 7 and no more than 30 days from now!");
+        validator.validate(LocalDate.now(), LocalDate.now().plusDays(6), 12345l);
+        validator.validate(LocalDate.now(), LocalDate.now().plusDays(31), 12345l);
+    }
+
+
+    @Test
+    public void shouldThrowExceptionWhenLibraryLicketIsNull() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Library Ticket ID must be not empty!");
+        validator.validate(LocalDate.now(), LocalDate.now().plusDays(8), null);
+    }
+
 
 
 }
