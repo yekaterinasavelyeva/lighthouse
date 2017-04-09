620f8c63d37b49ef89dce56ce7432d430c127691
Jekaterina Saveljeva <katya-a@inbox.lv>
2017-04-09 18:37:12 +0300

    add test for Reservation Factory

--- src/test/java/lv/javaguru/java2/services/reservation/ReservationFactoryImplTest.java
+++ src/test/java/lv/javaguru/java2/services/reservation/ReservationFactoryImplTest.java
@@ -1,12 +1,34 @@
 package lv.javaguru.java2.services.reservation;
 
 
+import lv.javaguru.java2.database.ReservationDAO;
+import lv.javaguru.java2.database.jdbc.ReservationDAOImpl;
+import lv.javaguru.java2.domain.Reservation;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by user on 09.04.2017.
  */
 public class ReservationFactoryImplTest {
 
+    ReservationFactory factory = new ReservationFactoryImpl();
+    private ReservationDAO reservationDAO = new ReservationDAOImpl();
 
+    @Test
+    public void createReservationTest() {
+        Reservation reservation = factory.create(LocalDate.now(), LocalDate.parse("2017-04-19"), 12345l);
+        Optional<Reservation> reservationFromDB = reservationDAO.getByID(reservation.getReservationID());
+        assertTrue(reservationFromDB.isPresent());
+        assertEquals(reservation.getReservationID(), reservationFromDB.get().getReservationID());
+        assertEquals(reservation.getDateFrom(), reservationFromDB.get().getDateFrom());
+        assertEquals(reservation.getDateTo(), reservationFromDB.get().getDateTo());
+        assertEquals(reservation.getLibTicketID(), reservationFromDB.get().getLibTicketID());
+    }
 
 }
