0fdba29ef44e8f58779cd8eb9bb1ec3f7f2e6d69
VBarbasins <barbasins@inbox.lv>
2017-04-25 10:59:14 +0300

    Added validation for DeleteResourceService. Test updated.

--- src/main/java/lv/javaguru/java2/services/resource/DeleteResourceServiceImpl.java
+++ src/main/java/lv/javaguru/java2/services/resource/DeleteResourceServiceImpl.java
@@ -4,10 +4,13 @@ import lv.javaguru.java2.database.ReservationDAO;
 import lv.javaguru.java2.database.ResourceDAO;
 import lv.javaguru.java2.database.jdbc.ReservationDAOImpl;
 import lv.javaguru.java2.database.jdbc.ResourceDAOImpl;
+import lv.javaguru.java2.domain.Reservation;
+import lv.javaguru.java2.domain.ReservationStatus;
 import lv.javaguru.java2.domain.Resource;
 import lv.javaguru.java2.services.resource.validate.ResourceIdValidator;
 import lv.javaguru.java2.services.resource.validate.ResourceIdValidatorImpl;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -28,13 +31,28 @@ public class DeleteResourceServiceImpl implements DeleteResourceService {
    @Override
    public void deleteResource(Long resourceId){
        resourceIdValidator.validate(resourceId);
-       Optional<Resource> userAccOpt = resourceDAO.getByID(resourceId);
-       if (!userAccOpt.isPresent()) {
-           throw new IllegalArgumentException("Resource not found by id = " + resourceId);
-       }
+       validateResourceExist(resourceId);
+       validateReservationsForResourceAreClosed(resourceId);
        resourceDAO.delete(resourceId);
        printMessage("Resource with id " + resourceId + " was deleted.");
    }
+
+    private void validateResourceExist(Long resourceId) {
+        Optional<Resource> resourceFromDB = resourceDAO.getByID(resourceId);
+        if (!resourceFromDB.isPresent()) {
+            throw new IllegalArgumentException("Resource not found by id = " + resourceId);
+        }
+    }
+
+    private void validateReservationsForResourceAreClosed(Long resourceId) {
+        List<Reservation> reservationsFromDB = reservationDAO.getByResourceID(resourceId);
+        boolean thereAreOpenedReservations = false;
+        thereAreOpenedReservations = reservationsFromDB.stream()
+                .anyMatch(reservation -> reservation.getStatus().equals(ReservationStatus.OPEN));
+        if (thereAreOpenedReservations) {
+            throw new IllegalArgumentException("There are opened reservations for this resource");
+        }
+    }
 
     public void printMessage(String message){
        System.out.println(message);
--- src/test/java/lv/javaguru/java2/services/resource/DeleteResourceServiceImplTest.java
+++ src/test/java/lv/javaguru/java2/services/resource/DeleteResourceServiceImplTest.java
@@ -2,6 +2,8 @@ package lv.javaguru.java2.services.resource;
 
 import lv.javaguru.java2.database.ReservationDAO;
 import lv.javaguru.java2.database.ResourceDAO;
+import lv.javaguru.java2.domain.Reservation;
+import lv.javaguru.java2.domain.ReservationStatus;
 import lv.javaguru.java2.domain.Resource;
 import lv.javaguru.java2.services.resource.validate.ResourceIdValidator;
 import org.junit.Before;
@@ -11,6 +13,7 @@ import org.mockito.InOrder;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -61,6 +64,7 @@ public class DeleteResourceServiceImplTest {
         when(resourceDAO.getByID(resourceId)).thenReturn(Optional.of(resource));
         service.deleteResource(resourceId);
         inOrder.verify(deleteValidator).validate(resourceId);
+        inOrder.verify(reservationDAO).getByResourceID(resourceId);
         inOrder.verify(resourceDAO).delete(resourceId);
     }
 
@@ -71,6 +75,43 @@ public class DeleteResourceServiceImplTest {
         service.deleteResource(resource.getResourceID());
         List<Resource> resources = resourceDAO.getAll();
         assertFalse(resources.contains(resource));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionIfThereAreOpenedReservationsForThisResource(){
+        List<Reservation> reservations = new ArrayList<>();
+        Reservation reservationA = Mockito.mock(Reservation.class);
+        Reservation reservationB = Mockito.mock(Reservation.class);
+        reservations.add(reservationA);
+        reservations.add(reservationB);
+        when(resourceDAO.getByID(resourceId))
+                .thenReturn(Optional.of(resource));
+        when(reservationDAO.getByResourceID(resourceId))
+                .thenReturn(reservations);
+        when(reservationA.getStatus())
+                .thenReturn(ReservationStatus.CLOSED);
+        when(reservationB.getStatus())
+                .thenReturn(ReservationStatus.OPEN);
+        service.deleteResource(resourceId);
+    }
+
+    @Test
+    public void shouldDeleteResourceWhenAllReservationsForItAreClosed(){
+        List<Reservation> reservations = new ArrayList<>();
+        Reservation reservationA = Mockito.mock(Reservation.class);
+        Reservation reservationB = Mockito.mock(Reservation.class);
+        reservations.add(reservationA);
+        reservations.add(reservationB);
+        when(resourceDAO.getByID(resourceId))
+                .thenReturn(Optional.of(resource));
+        when(reservationDAO.getByResourceID(resourceId))
+                .thenReturn(reservations);
+        when(reservationA.getStatus())
+                .thenReturn(ReservationStatus.CLOSED);
+        when(reservationB.getStatus())
+                .thenReturn(ReservationStatus.CLOSED);
+        service.deleteResource(resourceId);
+        verify(resourceDAO).delete(resourceId);
     }
 
 
