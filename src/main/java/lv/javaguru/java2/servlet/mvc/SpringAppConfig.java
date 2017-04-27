19ede1b2c744f06422a47739a930998b718d02ae
Viktor Savonin <viktor.savonin@meawallet.com>
2017-04-27 16:18:45 +0300

    MVCFilter

---
+++ src/main/java/lv/javaguru/java2/servlet/mvc/SpringAppConfig.java
@@ -0,0 +1,11 @@
+package lv.javaguru.java2.servlet.mvc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"lv.javaguru.java2"})
+public class SpringAppConfig {
+
+
+}
